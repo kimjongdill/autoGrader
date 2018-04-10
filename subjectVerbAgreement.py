ccb72062245b257ad447f8f06942c2b50ce551b9
kimjongdill <george.a.dill@gmail.com>
2018-04-10 07:51:01 -0500

    Started on Subject - Verb Agreement. Seems to work for simple sentences. Expanding to work with compound.

--- subjectVerbAgreement.py
+++ subjectVerbAgreement.py
@@ -28,21 +28,21 @@ import re
 def preProcess(text):
     text = text.replace(".", ". ")
     text = text.replace(",", ", ")
-    text = text.casefold()
     text = text.replace(" i ", " I ")
     return text;
 
 # Dec agreement by tracking subjects and then first occurance of verb
 
 def decAgreement(sentence):
-    wordTokens = nltk.word_tokenize(sentence)
-    pos = nltk.pos_tag(wordTokens)
+    tokens = nltk.word_tokenize(sentence)
+    pos = nltk.pos_tag(tokens)
 
     subjects= []
     headVerb = False;
     agreement = True
 
     for tag in pos:
+        tag = (tag[0].casefold(), tag[1])
         print(tag)
 
         if tag[1] == "NN" or tag[1] == "NNP" or  tag[1] == "NNS" or \
@@ -67,19 +67,20 @@ def decAgreement(sentence):
 
             # Check for third person
             for subject in subjects:
-                if subject[0] == "I" or subject[0] == "we" or subject[0] == "they":
+                if subject[0] == "i" or subject[0] == "we" or subject[0] == "they":
                     return False;
 
         # Non Third Person Singular
-        if tag[1] == "VBP" or tag[1] == "VB":
+        if tag[1] == "VBP":
             headVerb = True
             if len(subjects) is 0:
                 return False;
-            if tag[0] == "am" and not subjects[0][0] == "I" and len(subjects) > 1:
+            if tag[0] == "am" and not subjects[0][0] == "i" and len(subjects) > 1:
                 return False;
-            if tag[0] == "are" and subjects[0][0] == "I" and len(subjects) == 1:
+            if tag[0] == "are" and subjects[0][0] == "i" and len(subjects) == 1:
                 return False;
-
+            if "s" in tag[0][-1]:
+                return False;
 
     return agreement
 

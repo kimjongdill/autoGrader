0d80bcc5be729d08773b2e761f6c88e03e3f969b
hmx789 <hassanm789@gmail.com>
2018-04-28 14:13:33 -0500

    Fixed, now should be able to handle all essays.

--- src/SentenceStructure.py
+++ src/SentenceStructure.py
@@ -13,75 +13,69 @@ class SentenceStructure():
 
     # Comb through text and ensure that periods and commas have a space after them.
     def preProcess(self, text):
-        text = text.replace(".", ". ")
+        #text = text.replace(".", ". ")     # This will cause an error when parsing
         text = text.replace(",", ", ")
         text = text.replace(" i ", " I ")
         text = text.replace("  ", " ")
         return text;
 
 
-    def scoreFormCounts(self,essay):
+    def scoreFormCounts(self,processedEssay):
+        processedEssay = self.preProcess(processedEssay)
         formCount = 0
-        processedEssay = self.preProcess(essay)
         sawSubject = 0
         sawVerb = 0
         sentences = nltk.sent_tokenize(processedEssay)
         for sentence in sentences:
             sentence = sentence.replace("."," ")        #get different results when i get rid of the period
-            try:
-                (parse,) = self.parser.raw_parse(sentence)
-                #parse.pretty_print()
-            except:
-                parse = ""
+            (parse,) = self.parser.raw_parse(sentence)
+            #parse.pretty_print()
 
-            try:
-                if parse[0]._label == 'SBAR':           # if the root is an SBAR then it is incomplete
-                    #print "Found a SBAR",sentence
-                    formCount = formCount + 1
-            
-                elif parse[0]._label == 'FRAG':         # if the root is a fragment it is incomplete
-                    #print "Found FRAG",sentence
-                    formCount = formCount + 1
+            if parse[0]._label == 'SBAR':           # if the root is an SBAR then it is incomplete
+                #print "Found a SBAR",sentence
+                formCount = formCount + 1
 
-                elif parse[0]._label == 'PP':           # If the root is a PP, (For the bus all morning)
-                    #print "Found PP",sentence
-                    formCount = formCount + 1
+            elif parse[0]._label == 'FRAG':         # if the root is a fragment it is incomplete
+                #print "Found FRAG",sentence
+                formCount = formCount + 1
 
-                elif parse[0]._label == 'SINV':
-                    formCount = formCount + 1
+            elif parse[0]._label == 'PP':           # If the root is a PP, (For the bus all morning)
+                #print "Found PP",sentence
+                formCount = formCount + 1
 
-                subtreeLabels = []
-                subtrees = parse.subtrees()
-                for subtree in subtrees:
-                    subtreeLabels.append(subtree.label())
+            elif parse[0]._label == 'SINV':         #
+                formCount = formCount + 1
 
-                labelLength = len(subtreeLabels)
-                if subtreeLabels[labelLength-1] in ['CC','IN','DT']:
-                    #print "Found CC or IN at end"
-                    formCount = formCount + 1
+            subtreeLabels = []
+            subtrees = parse.subtrees()
+            for subtree in subtrees:
+                subtreeLabels.append(subtree.label())
 
-                for i in range(labelLength):
+            labelLength = len(subtreeLabels)
+            if subtreeLabels[labelLength-1] in ['CC','IN','DT']:
+                #print "Found CC or IN at end"
+                formCount = formCount + 1
 
-                    if subtreeLabels[i] == 'NP':
-                        sawSubject = 1
+            for i in range(labelLength):
 
-                # if a full stop is followed by a CC or iN then incomplete
-                    if subtreeLabels[i] == '.':
-                        if subtreeLabels[i-1] in ['CC','IN']:
-                            formCount = formCount + 1
+                if subtreeLabels[i] == 'NP':            #Looking for a subject
+                    sawSubject = 1
 
-                # Had I the wings of a bird
-                    if subtreeLabels[i] in ['VBD','VBP','VB','VBZ','VBN','VBG']:
-                        sawVerb = 1
-                        if sawSubject == 0:
-                            #print "Got a verb before a subject"
-                            formCount = formCount + 1
-                if sawVerb == 0:
-                    formCount = formCount + 1
+            # if a full stop is followed by a CC or iN then incomplete
+                if subtreeLabels[i] == '.':
+                    if subtreeLabels[i-1] in ['CC','IN']:
+                        formCount = formCount + 1
 
-            except:
-                print ("null")
-            #print formCount
+            # Had I the wings of a bird
+                if subtreeLabels[i] in ['VBD','VBP','VB','VBZ','VBN','VBG']:
+                    sawVerb = 1
+                    if sawSubject == 0:             #Checking to see if a verb occurs before a subject is given
+                        #print "Got a verb before a subject"
+                        formCount = formCount + 1
+            if sawVerb == 0:                    #If we have not seen a verb, then the sentence can't be proper
+                formCount = formCount + 1
+
+
 
         return formCount
 
@@ -96,40 +90,30 @@ if __name__ == "__main__":
     # Ensure there are spaces between periods and commas. This may not work so well for elipses,
     # But those shouldn't really be in academic writing...
     sentenceForm = SentenceStructure()
-    file = "../input/testing/essays/" + "1414676.txt"
 
-    essayFile = open(file, 'r')
-    essay = essayFile.read()
-    count = sentenceForm.scoreFormCounts(essay)
-    #count = sentenceForm.scoreDParser(userSentence)
-
-    #toc = open("../input/testing/index.csv", 'r')
+    toc = open("../input/testing/index.csv", 'r')
 
     # Import the table of contents
-    #lines = toc.readlines()
+    lines = toc.readlines()
 
     # Skip the title line
-    # lines.pop(0)
-    # for line in lines:
-    #     line = line.split(';')
-    #     print(line)
-    #     file = "../input/testing/essays/" + line[0]
-    #     score = line[2]
-    #
-    #     if "high" in score:
-    #         score = 1
-    #     else:
-    #         score = 0
-    #
-    #     essayFile = open(file, 'r')
-    #     essay = essayFile.read()
-    #
-    #     count = sentenceForm.scoreFormCounts(essay)
-    #
-    #     plt1.scatter(count, score)
-    #
-    # plt1.show()
+    lines.pop(0)
+
+    for line in lines:
+         line = line.split(';')
+         print(line)
+         file = "../input/testing/essays/" + line[0]
+         score = line[2]
+         print line[0]
+
+         if "high" in score:
+             score = 1
+         else:
+             score = 0
+
+         essayFile = open(file, 'r')
+         essay = essayFile.read()
+         count = sentenceForm.scoreFormCounts(essay)
 
 
-    #print "Sentence syntax errors",count
     print("Exited")
